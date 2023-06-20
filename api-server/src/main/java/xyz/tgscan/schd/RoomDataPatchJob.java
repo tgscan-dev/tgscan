872cf9128e482052b17f3927ee73f864258be39a
tgscan-dev <tgscan.xyz@outlook.com>
2023-06-20 20:02:45 +0800

    chore(data_path): de-dup room

---
+++ api-server/src/main/java/xyz/tgscan/schd/RoomDataPatchJob.java
@@ -0,0 +1,41 @@
+package xyz.tgscan.schd;
+
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import xyz.tgscan.domain.Room;
+import xyz.tgscan.repository.RoomRepository;
+
+@Component
+@Slf4j
+public class RoomDataPatchJob implements CommandLineRunner {
+  @Autowired private RoomRepository roomRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    new Thread(
+            () -> {
+              var all = roomRepository.findAll();
+              var collect =
+                  all.stream().collect(Collectors.groupingBy(room -> room.getLink().toLowerCase()));
+              var duplicate =
+                  collect.entrySet().stream().filter(x -> x.getValue().size() > 1).toList();
+              for (Map.Entry<String, List<Room>> entry : duplicate) {
+                entry.getValue().stream()
+                    .skip(1)
+                    .forEach(
+                        entity -> {
+                          roomRepository.delete(entity);
+                          log.info("delete duplicate room {}", entity.getId());
+                        });
+              }
+              log.info("done");
+            })
+        .start();
+  }
+}
