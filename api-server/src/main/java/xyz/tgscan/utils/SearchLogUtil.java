d94a87292207940f85ce376cf7a3f1cb218e3a8e
tgscan-dev <tgscan.xyz@outlook.com>
2023-07-17 22:54:55 +0800

    chore(crawl): save user_name as room entity

--- api-server/src/main/java/xyz/tgscan/utils/SearchLogUtil.java
+++ api-server/src/main/java/xyz/tgscan/utils/SearchLogUtil.java
@@ -4,19 +4,30 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import xyz.tgscan.domain.Room;
 import xyz.tgscan.domain.SearchLog;
+import xyz.tgscan.enums.TgRoomStatusEnum;
+import xyz.tgscan.repository.RoomRepository;
 import xyz.tgscan.repository.SearchLogRepository;
 
 @Slf4j
 @Component
 public class SearchLogUtil implements CommandLineRunner {
 
-  @Autowired private SearchLogRepository searchLogRepository;
   private final ArrayBlockingQueue<SearchLog> q = new ArrayBlockingQueue<>(2048);
+  @Autowired private SearchLogRepository searchLogRepository;
+  @Autowired private RoomRepository roomRepository;
+
+  public static boolean isValidGroupName(String input) {
+    String regex = "^@[a-zA-Z][a-zA-Z0-9_]{3,31}$";
+    return Pattern.matches(regex, input);
+  }
 
   public void log(String kw, String t, Integer p, String ip) {
     var searchAt = new Timestamp(System.currentTimeMillis());
@@ -37,6 +48,24 @@ public class SearchLogUtil implements CommandLineRunner {
                     els.add(log);
                   }
                   searchLogRepository.saveAll(els);
+                  var links =
+                      els.stream()
+                          .map(SearchLog::getKw)
+                          .filter(SearchLogUtil::isValidGroupName)
+                          .map(String::toLowerCase)
+                          .distinct()
+                          .map(x -> "https://t.me/" + x.substring(1))
+                          .toList();
+                  @SuppressWarnings("DuplicatedCode")
+                  var has =
+                      roomRepository.findByLinkIn(links).stream()
+                          .map(Room::getLink)
+                          .collect(Collectors.toSet());
+                  var notHas = links.stream().filter(x -> !has.contains(x)).toList();
+                  roomRepository.saveAll(
+                      notHas.stream()
+                          .map(x -> new Room().setLink(x).setStatus(TgRoomStatusEnum.NEW.name()))
+                          .toList());
                 } catch (InterruptedException e) {
                   e.printStackTrace();
                 }
