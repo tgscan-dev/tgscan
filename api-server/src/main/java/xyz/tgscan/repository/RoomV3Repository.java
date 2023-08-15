537334c37bed8d0e7915867385c160b99ecf653c
tgscan-dev <tgscan.xyz@outlook.com>
2023-08-15 16:14:48 +0800

    Remove useless code

--- api-server/src/main/java/xyz/tgscan/repository/RoomV3Repository.java
+++
@@ -1,9 +0,0 @@
-package xyz.tgscan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.tgscan.domain.RoomV3;
-
-public interface RoomV3Repository extends JpaRepository<RoomV3, Long> {
-
-
-}
